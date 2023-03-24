54f50a35a672d269d7a0f5e1c511c9b34c2d7425
bb <bb@pop-tiny.lan>
2023-03-24 16:53:58 +0000

    More packages, what can I say...

--- .custom.el
+++ .custom.el
@@ -8,7 +8,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(beframe-mode sxhkdrc-mode beframe lin substitute vimish-fold flymake-shellcheck haskell-mode yuck-mode lua-mode pdf-tools minions chezmoi prog-mode bicycle zig-mode java-imports keycast direnv proof-general tuareg python-mode pyimport modus-themes attrap fish-mode diredfl meson-mode gnu-elpa-keyring-update c++-mode logview shfmt dhall-mode v-mode svelte-mode json-mode yaml-mode racket-mode fennel-mode cmake-mode org-anki cpp-auto-include avy srefactor-lisp clang-capf editorconfig magit-annex neotree org-modern magit mmm-mode rainbow-mode ef-themes go-dlv highlight-indent-guides go-mode kind-icon expand-region beacon consult-eglot eglot consult-yasnippet yasnippet cape corfu embark-consult embark marginalia orderless consult vertico which-key auto-package-update use-package)))
+   '(nix-mode poetry flymake-racket chemtable vterm beframe sxhkdrc-mode lin substitute vimish-fold flymake-shellcheck haskell-mode yuck-mode lua-mode pdf-tools minions chezmoi prog-mode bicycle zig-mode java-imports keycast direnv proof-general tuareg python-mode pyimport modus-themes attrap fish-mode diredfl meson-mode gnu-elpa-keyring-update c++-mode logview shfmt dhall-mode v-mode svelte-mode json-mode yaml-mode racket-mode fennel-mode cmake-mode org-anki cpp-auto-include avy srefactor-lisp clang-capf editorconfig magit-annex neotree org-modern magit mmm-mode rainbow-mode ef-themes go-dlv highlight-indent-guides go-mode kind-icon expand-region beacon consult-eglot eglot consult-yasnippet yasnippet cape corfu embark-consult embark marginalia orderless consult vertico which-key auto-package-update use-package))
+ '(warning-suppress-types '((comp))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
