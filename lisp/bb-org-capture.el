31b6574b3b1d4f58266e6bec9c97f2aba8f0c617
bb <bb@pop-tiny.lan>
2023-03-24 16:52:49 +0000

    Adventures in org templates

--- lisp/bb-org-capture.el
+++ lisp/bb-org-capture.el
@@ -1,24 +1,35 @@
 (setq org-capture-templates
    '(("p" "Personal")
      ("f" "Family")
-     ("s" "Special Event")
+     ("s" "Special Event" entry
+      (file+headline "notes.org" "SPECIAL EVENT")
+      (file "templates/special_event")
+      :empty-lines 1)
      ("r" "Repairs/Maintenance")
      ("w" "Work")
      ("pj" "Journal entry" entry
-      (file+olp+datetree "journal.org")
+      (file+olp+datetree "notes.org")
       (file "templates/p_journal"))
-     ("pl" "Linux" entry
-      (file+headline "personal.org" "LINUX topic CAPTURE")
-      (file "templates/p_linux"))
+     ("ph" "Code snippet" entry
+      (file+headline "notes.org" "CODE --> CAPTURE")
+      (file "templates/p_code_snippet"))
+     ("pt" "Task with deadline" entry
+      (file+headline "notes.org" "DEADLINE --> CAPTURE")
+      (file "templates/p_todo_deadline"))
+     ("pa" "Acquisitions" entry
+      (file+headline "notes.org" "PURCHASES --> CAPTURE")
+      (file "templates/p_purchases"))
+     ("pb" "Borrowed stuff" entry
+      (file+headline "notes.org" "BORROWED --> CAPTURE")
+      (file "templates/p_borrowed")
+      :empty-lines 1)
      ("ft" "Requests with date" entry
-      (file+headline "family.org" "TODO family CAPTURE")
+      (file+headline "notes.org" "Family related")
       (file "templates/f_todo"))
-     ("pb" "This is a catch_all" entry
-      (file+headline "catch_all.org" "Some catch all heading")
-      "* SOMEDAY")
-     ("pt" "TODO with deadline" entry
-      (file+heading "personal.org" "TODO personal CAPTURE ")
-	    (file "templates/p_todo_deadline"))
+     ("rt" "Tools and others" entry
+      (file+headline "notes.org" "Maintenance")
+      (file "templates/r_tools")
+      :empty-lines 1)
      ))
 
 (provide 'bb-org-capture)
