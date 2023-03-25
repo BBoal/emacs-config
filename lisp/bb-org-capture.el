a46e8dd511a18b450b16872e062d9958fc7a6985
bb <bb@pop-tiny.lan>
2023-03-25 02:26:01 +0000

    Proper headings. Added LICENSE and README

--- lisp/bb-org-capture.el
+++ lisp/bb-org-capture.el
@@ -1,3 +1,29 @@
+;;; bb-org-capture.el --- Personal Org Capture Templates -*- lexical-binding: t -*-
+
+;; Copyright (c) 2023  Bruno Boal <bruno.boal@tutanota.com>
+;; Author: Bruno Boal <bruno.boal@tutanota.com>
+;; URL: https://github.com/BBoal/emacs-config
+;; Package-Requires: ((emacs "28.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;; Code:
+
+
 (setq org-capture-templates
    '(("p" "Personal")
      ("f" "Family")
