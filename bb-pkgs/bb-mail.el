4d58f6707ae63d38076dae3bed0b1d9bf4043f20
bboal <egomet@bboal.com>
2023-07-02 18:30:06 +0100

    Added notmuch-indicator package

--- bb-pkgs/bb-mail.el
+++ bb-pkgs/bb-mail.el
@@ -7,7 +7,6 @@
 
 ;;;; `smtpmail'
 (use-package smtpmail
-  :defer 1
   :config
   (setq smtpmail-default-smtp-server "smtp.mailbox.org"
         smtpmail-smtp-server "smtp.mailbox.org"
@@ -25,12 +24,25 @@
 
 ;;;; `notmuch'
 (use-package notmuch
-  :defer 1
   :load-path "/usr/share/emacs/site-lisp/"
   :config
   (setq notmuch-identities '("Bruno Boal <egomet@bboal.com>")
         notmuch-fcc-dirs   '(("egomet@bboal.com" . "mailbox/Sent"))
         notmuch-show-logo nil))
+
+
+;;;; `notmuch-indicator'
+(use-package notmuch-indicator
+  :after (notmuch smtpmail)
+  :vc (:url "https://git.sr.ht/~protesilaos/notmuch-indicator"
+       :rev :newest)
+  :config
+  (setq notmuch-indicator-args
+        '((:terms "tag:unread" :label " U:" :face warning)))
+  (setq notmuch-indicator-refresh-count 300)
+  (setq notmuch-indicator-hide-empty-counters t)
+  (setq notmuch-indicator-force-refresh-commands '(notmuch-refresh-this-buffer))
+  (notmuch-indicator-mode 1))
 
 
 (provide 'bb-mail)
