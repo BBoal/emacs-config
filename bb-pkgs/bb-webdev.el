c57ffaad39023509e77b6ddb6b924619a0fe2ac7
bboal <egomet@bboal.com>
2023-07-02 17:53:58 +0100

    Removed unmaintained emmet-mode. Added emmet2-mode with deno backend.

--- bb-pkgs/bb-webdev.el
+++ bb-pkgs/bb-webdev.el
@@ -5,14 +5,20 @@
 
 ;;; Code:
 
-;;;; `emmet-mode'
-(use-package emmet-mode
-  :defer 2
-  :hook (sgml-mode html-mode css-mode html-ts-mode)
-  :config
-  (setq emmet-move-cursor-between-quotes t))
 
+(use-package websocket
+  :demand t)
 
+(use-package deno-bridge
+  :after websocket
+  :vc ( :url "https://github.com/manateelazycat/deno-bridge"
+        :rev :newest))
+
+(use-package emmet2-mode
+  :after deno-bridge
+  :vc ( :url "https://github.com/P233/emmet2-mode"
+        :rev :newest)
+  :hook (mhtml-mode html-ts-mode sgml-mode css-mode))
 
 
 (provide 'bb-webdev)
