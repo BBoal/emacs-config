1ff9e17fb7b9eff96b957464afcbbb61cb900ef0
bboal <egomet@bboal.com>
2023-07-02 19:28:59 +0100

    Major overhaul with modeline in session with Prot

--- bb-pkgs/bb-ui-ux.el
+++ bb-pkgs/bb-ui-ux.el
@@ -6,6 +6,12 @@
 ;;; Code:
 
 
+(setq modus-operandi-tinted-palette-overrides
+      '((cursor blue-intense)
+        (bg-mode-line-active "#d0d6ff")
+        (bg-mode-line-inactive "#e6e6e6")))
+
+
 ;;;; `hl-todo'
 (use-package hl-todo
   :defer 3
@@ -49,27 +55,212 @@
   :defer 2
   :bind (:map ctl-x-x-map
               ("c" . rainbow-mode))
-  :hook ((css-mode html-mode sass-mode) . rainbow-mode)
+  :hook ((css-mode
+          css-ts-mode
+          html-mode
+          html-ts-mode
+          js-mode
+          js-ts-mode) . rainbow-mode)
   :config
   (setq rainbow-ansi-colors nil
         rainbow-x-colors nil))
+
+
+;;;; `modus-themes'
+(use-package modus-themes
+  :demand t
+  :after keycast
+  :config
+  (load-theme hour-sets-modus))
+
+
+;;;; `ef-themes'
+(use-package ef-themes)
+
+
+;;;; `modeline-setup'
+(defvar bb-modeline-major-mode
+  (list (propertize "%[" 'face 'error)
+        '(:eval
+          (propertize
+           (capitalize
+            (replace-regexp-in-string
+             "-mode"
+             ""
+             (symbol-name major-mode)))
+           'mouse-face 'mode-line-highlight))
+        '("" mode-line-process)
+        (propertize "%]" 'face 'error)
+        " ")
+  "Mode line construct for displaying major modes.")
+
+(defvar prot-modeline-vc-branch
+  '(:eval
+    (when-let* (((mode-line-window-selected-p))
+                (branches (vc-git-branches))
+                (branch (car branches))
+                (state (vc-state (buffer-file-name) 'Git))
+                (face (pcase state
+                        ('added 'vc-locally-added-state)
+                        ('edited 'vc-edited-state)
+                        ('removed 'vc-removed-state)
+                        ('missing 'vc-missing-state)
+                        ('conflict 'vc-conflict-state)
+                        ('locked 'vc-locked-state)
+                        (_ 'vc-up-to-date-state))))
+      (concat "   " (propertize (capitalize branch)
+                               'face face
+                               'mouse-face 'mode-line-highlight))))
+  "Mode line construct to return propertized VC branch.")
+
+
+(defvar prot-modeline-align-right
+  '(:eval (propertize
+           " " 'display
+           `((space :align-to
+                    (- (+ right right-fringe right-margin)
+                       ,(string-width
+                         (format-mode-line mode-line-misc-info)))))))
+  "Mode line construct to align following elements to the right.
+Read Info node `(elisp) Pixel Specification'.") ;; 2023-06-30  REMINDER => See this in detail
+
+
+(defvar prot-modeline-misc-info
+  '(:eval
+    (when (mode-line-window-selected-p)
+      mode-line-misc-info))
+  "Mode line construct displaying `mode-line-misc-info'.
+Specific to the current window's mode line.")
+
+
+(defface prot-modeline-intense
+  '((default :inherit bold)
+    (((class color) (min-colors 88) (background light))
+     :background "#0000aa" :foreground "#ffffff")
+    (((class color) (min-colors 88) (background dark))
+     :background "#77aaff" :foreground "#000000")
+    (t :inverse-video t))
+  "Face for intense mode line constructs.")
+
+
+(setq mode-line-defining-kbd-macro
+      (propertize " KMacro " 'face 'prot-modeline-intense))
+
+(defvar prot-modeline-kbd-macro
+  '(:eval
+    (when (and defining-kbd-macro (mode-line-window-selected-p))
+      mode-line-defining-kbd-macro))
+  "Mode line construct displaying `mode-line-defining-kbd-macro'.
+Specific to the current window's mode line.")
+
+
+(defvar prot-modeline-buffer-identification
+  (propertized-buffer-identification "%b")
+  "Mode line construct for identifying the buffer being displayed.")
+
+
+(defvar prot-modeline--line-and-column
+  `((line-number-mode
+     (column-number-mode
+      (column-number-indicator-zero-based
+       (:propertize
+        mode-line-position-column-line-format
+        ,@mode-line-position--column-line-properties)
+       (:propertize
+        (:eval (string-replace
+                "%c" "%C" (car mode-line-position-column-line-format)))
+        ,@mode-line-position--column-line-properties))
+      (:propertize
+	   mode-line-position-line-format
+       ,@mode-line-position--column-line-properties))
+     (column-number-mode
+      (:propertize
+       mode-line-position-column-format
+       ,@mode-line-position--column-line-properties)
+      (:propertize
+       (:eval (string-replace
+               "%c" "%C" (car mode-line-position-column-format)))
+       ,@mode-line-position--column-line-properties)))
+    " "
+    (:propertize
+     ("" mode-line-percent-position)
+     mouse-face mode-line-highlight)
+    " ")
+  "Mode line construct for formatting `prot-modeline-position'.")
+
+
+(defvar prot-modeline-position
+  '(:eval
+    (when (mode-line-window-selected-p)
+      prot-modeline--line-and-column))
+  "Mode line construct for the buffer position.")
+
+;; Line/Column Pos%
+(setq mode-line-position-column-line-format '("%l:%c "))
+(setq mode-line-percent-position '(-3 "%o"))
+(column-number-mode 1)
+
+
+(defvar prot-modeline-flymake
+  '(:eval
+    (when (and (bound-and-true-p flymake-mode)
+               (mode-line-window-selected-p))
+      ;;(flatten-tree (cons flymake-mode-line-format "  "))))
+      ;;flymake-mode-line-format))
+      (list flymake-mode-line-exception flymake-mode-line-counters " ")))
+  "Mode line construct displaying `flymake-mode-line-format'.
+Specific to the current window's mode line.")
+
+
+(dolist (variables '(bb-modeline-major-mode
+                     prot-modeline-align-right
+                     prot-modeline-buffer-identification
+                     prot-modeline-flymake
+                     prot-modeline-kbd-macro
+                     prot-modeline-misc-info
+                     prot-modeline-position
+                     prot-modeline-vc-branch))
+  (put variables 'risky-local-variable t))
+
+
+(setq-default mode-line-format
+              '("%e"
+                prot-modeline-kbd-macro
+                " "
+                mode-line-mule-info
+                mode-line-modified
+                mode-line-remote
+                " "
+                prot-modeline-buffer-identification
+                "  "
+                bb-modeline-major-mode
+                prot-modeline-flymake
+                " "
+                prot-modeline-position
+                prot-modeline-vc-branch
+                prot-modeline-align-right
+                prot-modeline-misc-info ;; everything else not defined particularly
+                mode-line-end-spaces))
+
+
+;; Clock in the modeline
+;;(setq display-time-interval 10)
+;;(setq display-time-default-load-average nil)
+(setq display-time-string-forms
+      '((capitalize (format-time-string "%a, %d %b %R "))))
+(display-time-mode 1)
 
 
 ;;;; `keycast'
 (use-package keycast
   :demand t
   :config
-  (defun prot/keycast-current-window-p ()
-    "Return non-nil if selected WINDOW modeline can show keycast."
-    (and (not (minibufferp))
-         (not (null mode-line-format))
-         (eq (selected-window) (old-selected-window))))
-  (setq keycast-mode-line-window-predicate #'prot/keycast-current-window-p)
-  (setq keycast-separator-width 1)
+  (setq keycast-mode-line-insert-after 'prot-modeline-vc-branch)
+  (setq keycast-mode-line-format "%3s%k%c%R")
+  (setq keycast-mode-line-window-predicate 'mode-line-window-selected-p)
   (setq keycast-mode-line-remove-tail-elements nil)
-  (if (not (keycast--mode-active-p))
-      (keycast-mode 1)
-    (keycast-mode -1))
+  (unless (keycast--mode-active-p)
+      (keycast-mode 1))
 
   (dolist (input '(self-insert-command
                    org-self-insert-command))
@@ -81,28 +272,7 @@
     (add-to-list 'keycast-substitute-alist `(,event nil))))
 
 
-;;;; `modus-themes'
-(use-package modus-themes
-  :demand t
-  :after keycast
-  :config
-  (load-theme hour-sets-modus)
-  ;; Clock in the modeline
-  (setq display-time-string-forms
-        '((propertize (concat " " 24-hours ":" minutes " ")
-                      'face #'keycast-key)))
-  (display-time-mode 1))
 
-;; tab-bar tweaks
-;; (setq tab-bar-format               ;; Emacs 28
-;;      '(tab-bar-format-tabs-groups
-;;        tab-bar-format-align-right
-;;        tab-bar-format-global))
-;;global-mode-string (TODO)
-
-
-;;;; `ef-themes'
-(use-package ef-themes)
 
 
 ;;;; `which-key'
@@ -160,21 +330,6 @@
   (setq kind-icon-use-icons t
         kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
-
-
-
-;;;; `minions'
-(use-package minions
-  :defer 2
-  :config
-  (setq minions-mode-line-lighter ";")
-  ;; NOTE: This will be expanded whenever I find a mode that should not
-  ;; be hidden
-  (setq minions-prominent-modes
-        (list 'defining-kbd-macro
-              'flymake-mode))
-  :init
-  (minions-mode t))
 
 
 
