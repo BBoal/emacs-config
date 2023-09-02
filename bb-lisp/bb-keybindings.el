567db26b77e0039c48a1449465618e728574bdc7
bboal <egomet@bboal.com>
2023-09-02 21:53:40 +0100

    Refactored and rearrangement of bindings and several prefix-map settings

--- bb-lisp/bb-keybindings.el
+++ bb-lisp/bb-keybindings.el
@@ -5,19 +5,45 @@
 
 ;;; Code:
 
-(keymap-set lisp-interaction-mode-map "C-j" #'bb-eval-print-current-sexp-lisp)
 
-(keymap-global-set "<escape>" #'bb-simple-keyboard-quit-dwim)
-
-;; (defvar-keymap testing-prefix-f-map
+;;;; Prefix map Template
+;; (defvar-keymap testing-prefix-motion-map
 ;;   "e" #'move-end-of-line)
-;; (define-key testing-prefix-map "f" testing-prefix-f-map)
+;; (define-key testing-prefix-map "s-m" testing-prefix-motion-map)
 
-(defvar-keymap s-x-prefix-map
-  "k" #'bb-simple-kill-current-buffer
-  "o" #'bb-delete-blank-lines-dwim)
-(keymap-global-set "s-x" s-x-prefix-map)
 
+
+;;;; "M-g" g-goto-prefix-map
+(defvar-keymap g-goto-prefix-map
+  "e"   #'consult-compile-error
+  "M-g" #'consult-goto-line     ;; orig. goto-line
+  "o"   #'consult-outline       ;; Alternative: consult-org-heading
+  "m"   #'consult-mark
+  "M-m" #'consult-global-mark
+  "i"   #'consult-imenu
+  "I"   #'consult-imenu-multi)
+(keymap-global-set "M-g" g-goto-prefix-map)
+
+
+
+;;;; "M-s" s-search-prefix-map
+(defvar-keymap s-search-prefix-map
+  "G" #'consult-git-grep
+  "L" #'consult-line-multi
+  "f" #'consult-find
+  "g" #'consult-grep
+  "k" #'consult-keep-lines
+  "l" #'consult-line
+  "m" #'consult-multi-occur
+  "o" #'occur
+  "r" #'consult-ripgrep
+  "s" #'consult-isearch-history
+  "u" #'consult-focus-lines)
+(keymap-global-set "M-s" s-search-prefix-map)
+
+
+
+;;;; "s-c" s-change-prefix-map
 (defvar-keymap s-change-prefix-map
   "i" #'bb-change-inside-char-pairs
   "a" #'bb-change-around-char-pairs
@@ -25,36 +51,64 @@
 (keymap-global-set "s-c" s-change-prefix-map)
 
 
+
+;;;; "s-e" s-eglot-prefix-map
+(with-eval-after-load 'eglot
+  (defvar-keymap s-eglot-prefix-map
+    "r" #'eglot-rename
+    "o" #'eglot-code-action-organize-imports
+    "h" #'eldoc
+    "f" #'eglot-code-action-quickfix
+    "i" #'eglot-code-action-inline)
+  (keymap-set eglot-mode-map "s-e" s-eglot-prefix-map))
+
+
+
+;;;; "s-h" s-help-prefix-map
 (defvar-keymap s-help-prefix-map
   "a" #'apropos-command
   "m" #'consult-man
   "B" #'embark-bindings) ;; alternative for \`describe-bindings\'
 (keymap-global-set "s-h" s-help-prefix-map)
 
+
+
+;;;; "s-o" s-org-prefix-map
 (defvar-keymap s-org-prefix-map
   "a" #'org-agenda
   "c" #'org-capture
   "l" #'org-store-link)
 (keymap-global-set "s-o" s-org-prefix-map)
 
-(keymap-global-set "C-c 0" #'kill-emacs)
-(keymap-global-set "C-c <delete>" #'delete-frame)
-(keymap-global-set "C-c l" #'dictionary-lookup-definition)
 
+
+;;;; "s-x" s-x-prefix-map
+(defvar-keymap s-x-prefix-map
+  "k" #'bb-simple-kill-current-buffer
+  "n" #'logos-narrow-dwim
+  "o" #'bb-delete-blank-lines-dwim)
+(keymap-global-set "s-x" s-x-prefix-map)
+
+
+
+;;;; Function keys
 (keymap-global-set "<f12>" #'save-buffer)
 (keymap-global-set "<f10>" #'save-buffers-kill-emacs)
 (keymap-global-set "<f9>" #'menu-bar-mode)
-(keymap-global-set "<f8>" (lambda () (interactive)
-                            (find-file "~/.emacs.d/init.el")))
+(keymap-global-set "<f8>" #'logos-focus-mode)
 (keymap-global-set "<f7>" #'bb-simple-cycle-display-line-numbers)
 (keymap-global-set "<f6>" #'whitespace-mode)
 (keymap-global-set "<f5>" #'keycast-mode-line-mode)
 (keymap-global-set "<f2>" #'bb-revert-buffer-no-confirm)
+(keymap-global-set "<f1>" (lambda () (interactive)
+                            (find-file "~/.emacs.d/init.el")))
 
 (keymap-global-set "s-<f5>" #'bb-set-desktop-file-for-save)
 (keymap-global-set "s-<f8>" #'bb-silent-desktop-read)
 
 
+
+;;;; Window management "s-..."
 (keymap-global-set "s-k" #'windmove-up)
 (keymap-global-set "s-j" #'windmove-down)
 (keymap-global-set "s-h" #'windmove-left)
@@ -65,13 +119,11 @@
 (keymap-global-set "s-q" #'bb-kill-buffer-and-delete-window)
 (keymap-global-set "s-!" #'delete-other-windows-vertically)
 
-(keymap-global-set "M-s o" #'occur)
-(keymap-global-set "C-M-=" #'count-words)
 
 
+;;;; Text manipulation
 (keymap-global-set "M-k" #'bb-kill-beg-line)
 (keymap-global-set "M-t" #'bb-transpose-words)
-(keymap-global-set "M-/" #'hippie-expand)
 (keymap-global-set "M-u" #'upcase-dwim)
 
 (keymap-global-set "C-o" #'bb-insert-newline-below)
@@ -80,15 +132,14 @@
 (keymap-global-set "s-y" #'bb-kill-ring-save-line)
 (keymap-global-set "s-D" #'bb-duplicate-line-above-dwim)
 (keymap-global-set "s-d" #'bb-duplicate-line-below-dwim)
-(keymap-global-set "s-t" #'vterm-other-window)
+(keymap-global-set "s-," #'logos-backward-page-dwim)
+(keymap-global-set "s-." #'logos-forward-page-dwim)
 
 (keymap-global-set "C-+" #'bb-push-mark-no-activate)
 (keymap-global-set "M-+" #'bb-jump-to-mark)
-;; (define-key global-map [remap exchange-point-and-mark] #'exchange-point-and-mark-no-activate)
 
 (keymap-global-set "C-c n" #'bb-find-occurrence-direction-kill-sexp)
 (keymap-global-set "C-c a" #'bb-find-occurrence-direction-kill-around-sexp)
-
 
 (keymap-global-set "s-z" #'bb-zap-from-char-to-end)
 (keymap-global-set "M-z" #'zap-up-to-char)
@@ -98,7 +149,6 @@
 (keymap-global-set "ç" #'bb-simple-ç-dwim)
 (keymap-global-set "Ç" #'bb-simple-Ç-dwim)
 
-
 (keymap-global-set "s-f" #'forward-to-word)
 (keymap-global-set "s-b" #'backward-to-word)
 (keymap-global-set "s-*" #'isearch-forward-thing-at-point)
@@ -106,17 +156,20 @@
 (keymap-global-set "s-n" #'bb-move-line-below-dwim)
 
 
+
+;;;; notmuch
+(with-eval-after-load 'notmuch
+  (keymap-set notmuch-search-mode-map "/" #'notmuch-search-filter)           ; alias for l
+  (keymap-set notmuch-search-mode-map "r" #'notmuch-search-reply-to-thread)  ; easier to reply to all by default
+  (keymap-set notmuch-search-mode-map "R" #'notmuch-search-reply-to-thread-sender)
+  (keymap-set notmuch-show-mode-map "r" #'notmuch-show-reply)
+  (keymap-set notmuch-show-mode-map "R" #'notmuch-show-reply-sender))
+
 ;; (defvar-keymap s-notmuch-prefix-map
   ;; "m" #'notmuch-hello)
   ;; "u" #'notmuch-sort-saved-searches)
 (keymap-global-set "s-m" #'notmuch-hello) ;; s-notmuch-prefix-map)
 
-(with-eval-after-load 'notmuch
-  (keymap-set notmuch-search-mode-map "/" #'notmuch-search-filter) ; alias for l
-  (keymap-set notmuch-search-mode-map "r" #'notmuch-search-reply-to-thread) ; easier to reply to all by default
-  (keymap-set notmuch-search-mode-map "R" #'notmuch-search-reply-to-thread-sender)
-  (keymap-set notmuch-show-mode-map "r" #'notmuch-show-reply)
-  (keymap-set notmuch-show-mode-map "R" #'notmuch-show-reply-sender))
 
 
 ;;;; dired
@@ -125,5 +178,22 @@
   (keymap-unset dired-jump-map "j" :remove))
 
 
+
+;;;; other bindings
+(keymap-global-set "s-t" #'vterm-other-window)
+(keymap-global-set "M-/" #'hippie-expand)
+(keymap-global-set "C-M-=" #'count-words)
+(keymap-global-set "C-c 0" #'kill-emacs)
+(keymap-global-set "C-c <delete>" #'delete-frame)
+(keymap-global-set "C-c l" #'dictionary-lookup-definition)
+(keymap-set lisp-interaction-mode-map "C-j" #'bb-eval-print-current-sexp-lisp)
+;; (keymap-set minibuffer-local-map "C-s" #'consult-history)
+
+;; smart <Esc> key in context
+;; Thanks to Prot for showing me how to do this.
+(keymap-global-set "<escape>" #'bb-simple-keyboard-quit-dwim)
+
+
 (provide 'bb-keybindings)
+
 ;;; bb-keybindings.el ends here
