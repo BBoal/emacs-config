16ce0b608c6b8e9704ab2ba2bd9ae21d6e8e8850
bboal <egomet@bboal.com>
2023-09-02 21:52:22 +0100

    Got rid of embark. Changed consult bindings to dedicated file.

--- bb-pkgs/bb-smart-completion.el
+++ bb-pkgs/bb-smart-completion.el
@@ -24,6 +24,7 @@
 
 
 
+
 ;;;; `jinx'
 (use-package jinx
   :after vertico
@@ -47,39 +48,38 @@
 
 
 
+
 ;;;; `consult'
 (use-package consult
   :demand t
-  ;; Replace bindings. Lazily loaded due by `use-package'.
   :bind (;; C-c bindings (mode-specific-map)
          ("C-c s" . consult-history)
          ("C-c M-x" . consult-mode-command)
          ("C-c k" . consult-kmacro)
          ;; C-x bindings (ctl-x-map)
-         ("C-x M-:" . consult-complex-command)   ;; orig. repeat-complex-command
-         ("C-x b" . consult-buffer)              ;; orig. switch-to-buffer
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
          ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
          ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-         ("C-x r b" . consult-bookmark)        ;; orig. bookmark-jump
-         ("C-x p b" . consult-project-buffer)  ;; orig. project-switch-to-buffer
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
          ;; Custom M-# bindings for fast register access
          ("M-#" . consult-register-load)
-         ("M-'" . consult-register-store)  ;; orig. abbrev-prefix-mark (unrelated)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
          ("C-M-#" . consult-register)
          ;; Other custom bindings
-         ("M-y" . consult-yank-pop)     ;; orig. yank-pop
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
          ;; Minibuffer history
          :map minibuffer-local-map
-         ("M-s" . consult-history)       ;; orig. next-matching-history-element
-         ("M-r" . consult-history))      ;; orig. previous-matching-history-element
+         ("C-s" . consult-history))
+         ;; ("M-s" . consult-history)       ;; orig. next-matching-history-element
+         ;; ("M-r" . consult-history))      ;; orig. previous-matching-history-element
 
   ;; Enable automatic preview at point in the *Completions* buffer. This is
   ;; relevant when you use the default completion UI.
   :hook (completion-list-mode . consult-preview-at-point-mode)
 
-  ;; The :init configuration is always executed (Not lazy)
   :init
-
   ;; Optionally configure the register formatting. This improves the register
   ;; preview for `consult-register', `consult-register-load',
   ;; `consult-register-store' and the Emacs built-ins.
@@ -94,34 +94,7 @@
   (setq xref-show-xrefs-function #'consult-xref
         xref-show-definitions-function #'consult-xref)
 
-  ;; Configure other variables and modes in the :config section,
-  ;; after lazily loading the package.
   :config
-  (defvar-keymap m-search-prefix-map
-    "f" #'consult-find
-    "g" #'consult-grep
-    "G" #'consult-git-grep
-    "r" #'consult-ripgrep
-    "l" #'consult-line
-    "L" #'consult-line-multi
-    "m" #'consult-multi-occur
-    "k" #'consult-keep-lines
-    "u" #'consult-focus-lines
-    "s" #'consult-isearch-history)
-  (keymap-global-set "M-s" m-search-prefix-map)
-
-  (defvar-keymap g-goto-prefix-map
-    "e"   #'consult-compile-error
-    "M-g" #'consult-goto-line     ;; orig. goto-line
-    "o"   #'consult-outline       ;; Alternative: consult-org-heading
-    "m"   #'consult-mark
-    "M-m" #'consult-global-mark
-    "i"   #'consult-imenu
-    "I"   #'consult-imenu-multi)
-  (keymap-global-set "M-g" g-goto-prefix-map)
-
-  (keymap-set minibuffer-local-map "C-s" #'consult-history)
-
   ;; Setting the correct path to the config variable in order to get rid
   ;; of ripgrep errors during execution
   (setenv "RIPGREP_CONFIG_PATH" "/home/bb/.config/ripgrep/ripgreprc")
@@ -152,6 +125,8 @@
   )
 
 
+
+
 ;;;; `orderless'
 (use-package orderless
   :demand t
@@ -161,38 +136,46 @@
         completion-category-overrides '((file (styles basic partial-completion orderless)))))
 
 
+
+
 ;;;; `marginalia'
 (use-package marginalia
   :init
   (marginalia-mode))
 
 
-;;;; `embark'
-(use-package embark
-  :defer 2
-  :bind
-  (("C-." . embark-act)                 ;; pick some comfortable binding
-   ("C-," . embark-dwim))                ;; good alternative: M-.
-  :init
-  ;; Optionally replace the key help with a completing-read interface
-  (setq prefix-help-command #'embark-prefix-help-command)
-  :config
-  ;; Hide the mode line of the Embark live/completions buffers
-  (add-to-list 'display-buffer-alist
-               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-                 nil
-                 (window-parameters (mode-line-format . none)))))
+
+;; 
+;; ;;;; `embark'
+;; (use-package embark
+;;   :defer 2
+;;   :bind
+;;   (("C-." . embark-act)                 ;; pick some comfortable binding
+;;    ("C-," . embark-dwim))                ;; good alternative: M-.
+;;   :init
+;;   ;; Optionally replace the key help with a completing-read interface
+;;   (setq prefix-help-command #'embark-prefix-help-command)
+;;   :config
+;;   ;; Hide the mode line of the Embark live/completions buffers
+;;   (add-to-list 'display-buffer-alist
+;;                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+;;                  nil
+;;                  (window-parameters (mode-line-format . none)))))
+;;
+;;
+;;
+;; 
+;; ;;;; `embark-consult'
+;; (use-package embark-consult
+;;   :defer 3 ; only necessary if you have the hook below
+;;   ;; if you want to have consult previews as you move around an
+;;   ;; auto-updating embark collect buffer
+;;   :hook
+;;   (embark-collect-mode . consult-preview-at-point-mode))
 
 
-;;;; `embark-consult'
-(use-package embark-consult
-  :defer 3 ; only necessary if you have the hook below
-  ;; if you want to have consult previews as you move around an
-  ;; auto-updating embark collect buffer
-  :hook
-  (embark-collect-mode . consult-preview-at-point-mode))
 
-
+
 ;;;; `corfu'
 (use-package corfu
   :bind (:map corfu-map
@@ -206,25 +189,25 @@
     "Enable Corfu in the minibuffer if Vertico is not active.
 Useful for prompts such as `eval-expression' and `shell-command'."
     (unless (bound-and-true-p vertico--input)
-      (setq-local corfu-echo-delay nil   ;; Disable automatic echo and popup
+      (setq-local corfu-echo-delay nil     ;; Disable automatic echo and popup
                   corfu-popupinfo-delay nil)
       (corfu-mode 1)))
 
   (setq corfu-min-width 40
         corfu-max-width 80
-        corfu-cycle t                ;; Enable cycling for `corfu-next/previous'
-        corfu-auto nil               ;; Enable auto completion
+        corfu-cycle t                      ;; Enable cycling for `corfu-next/previous'
+        corfu-auto nil                     ;; Enable auto completion
         corfu-auto-delay 1
         corfu-auto-prefix 3
         corfu-popupinfo-mode t
         corfu-popupinfo-delay nil
-        corfu-separator ?\s          ;; Orderless field separator
+        corfu-separator ?\s                ;; Orderless field separator
         corfu-quit-at-boundary 'separator  ;; Never quit at completion boundary
         corfu-quit-no-match 'separator     ;;  quit, even if there is no match
-        corfu-preview-current t      ;; Disable current candidate preview
-        corfu-preselect 'prompt      ;; Disable candidate preselection
-        corfu-on-exact-match nil     ;; Configure handling of exact matches
-        corfu-scroll-margin 2)       ;; Use scroll margin
+        corfu-preview-current t            ;; Disable current candidate preview
+        corfu-preselect 'prompt            ;; Disable candidate preselection
+        corfu-on-exact-match nil           ;; Configure handling of exact matches
+        corfu-scroll-margin 2)             ;; Use scroll margin
 
   ;; Recommended: Enable Corfu globally.
   ;; This is recommended since Dabbrev can be used globally (M-/).
@@ -232,36 +215,39 @@ Useful for prompts such as `eval-expression' and `shell-command'."
   (global-corfu-mode))
 
 
+
+
 ;;;; `dabbrev'
 (use-package dabbrev
-  ;; Swap M-/ and C-M-/
-  :bind (("M-/" . dabbrev-completion)
-         ("C-M-/" . dabbrev-expand))
   :config
   (setq dabbrev-ignored-buffer-regexps
         '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'")))
 
 
+
+
 ;;;; `abbrev'
 (require 'abbrev)
 
 (setq abbrev-file-name (locate-user-emacs-file "bb-personal-abbrevs")
       only-global-abbrevs nil)
 
-(dolist (hook '(text-mode-hook prog-mode-hook git-commit-mode-hook))
-  (add-hook hook #'abbrev-mode))
+(mapc (lambda (hook)
+        (add-hook hook #'abbrev-mode))
+      '(text-mode-hook prog-mode-hook git-commit-mode-hook))
 ;; no confirmation on using ’abbrev-file-name’ to save abbreviations.
 (remove-hook 'save-some-buffers-functions #'abbrev--possibly-save)
 
-(define-key abbrev-map (kbd "u")  #'unexpand-abbrev) ;; C-x a u
+;; undo abbrev
+(keymap-set abbrev-map "u"  #'unexpand-abbrev) ;; C-x a u
 
 (define-abbrev-table
   'global-abbrev-table '(("BB" "Bruno Boal")
                          ("bmail" "<egomet@bboal.com>")
-                         ("bberg" "https://codeberg.org/BBoal")
-                         ("bhut"  "https://git.sr.ht/~bboal")
-                         ("blab"  "https://gitlab.com/bboal")
-                         ("bhub"  "https://github.com/BBoal")))
+                         ("bberg" "<https://codeberg.org/BBoal>")
+                         ("bhut"  "<https://git.sr.ht/~bboal>")
+                         ("blab"  "<https://gitlab.com/bboal>")
+                         ("bhub"  "<https://github.com/BBoal>")))
 (with-eval-after-load 'message
   (define-abbrev-table
     'message-mode-abbrev-table '(("gm"  "Good morning,")
@@ -275,6 +261,8 @@ Useful for prompts such as `eval-expression' and `shell-command'."
                                  ("unt" "Cheers, until next time,\nBruno Boal"))))
 
 
+
+
 ;;;; `cape'
 (use-package cape
   ;; Bind dedicated completion commands
@@ -289,11 +277,20 @@ Useful for prompts such as `eval-expression' and `shell-command'."
          ("M-p a" . cape-abbrev)
          ("M-p &" . cape-sgml))
   :init
-  (dolist (functions '(cape-dabbrev cape-file cape-history cape-keyword
-                       cape-elisp-block cape-sgml cape-abbrev cape-symbol))
-    (add-to-list 'completion-at-point-functions functions)))
+  (mapc (lambda (functions)
+          (add-to-list 'completion-at-point-functions functions))
+        '(cape-dabbrev
+          cape-file
+          cape-history
+          cape-keyword
+          cape-elisp-block
+          cape-sgml
+          cape-abbrev
+          cape-symbol)))
 
 
+
+
 ;;;; `yasnippet'
 (use-package yasnippet
   :hook ((text-mode prog-mode) . yas-minor-mode)
@@ -307,24 +304,21 @@ Useful for prompts such as `eval-expression' and `shell-command'."
   (yas-reload-all))
 
 
+
+
 ;;;; `consult-yasnippet'
 (use-package consult-yasnippet
   :defer 1
   :after (consult yasnippet))
 
 
+
+
 ;;;; `eglot'
 (use-package eglot
+  :defer 1
   :after envrc
   :config
-  (defvar-keymap s-eglot-prefix-map
-  "r" #'eglot-rename
-  "o" #'eglot-code-action-organize-imports
-  "h" #'eldoc
-  "f" #'eglot-code-action-quickfix
-  "i" #'eglot-code-action-inline)
-  (keymap-set eglot-mode-map "s-e" s-eglot-prefix-map)
-
   (setq corfu-popupinfo-mode t
         corfu-popupinfo-delay 1.0
         eglot-sync-connect nil
