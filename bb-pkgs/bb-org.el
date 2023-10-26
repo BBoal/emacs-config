d18fadb93ba1110c42ff9b275edc972966fc0ce6
bboal <egomet@bboal.com>
2023-10-26 14:55:14 +0100

    Added Org related options

--- bb-pkgs/bb-org.el
+++ bb-pkgs/bb-org.el
@@ -24,7 +24,23 @@
           org-startup-indented t
           org-src-preserve-indentation t
           org-src-tab-acts-natively nil
-          org-edit-src-content-indentation 0))
+          org-auto-align-tags nil
+          org-tags-column 0
+          org-catch-invisible-edits 'show-and-error
+          org-special-ctrl-a/e t
+          org-insert-heading-respect-content t
+          org-edit-src-content-indentation 0
+          org-hide-emphasis-markers t
+          org-pretty-entities t
+          org-ellipsis "…"
+          org-agenda-tags-column 0
+          org-agenda-block-separator ?─
+          org-agenda-time-grid
+          '((daily today require-timed)
+            (800 1000 1200 1400 1600 1800 2000)
+            " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+          org-agenda-current-time-string
+          "◀── now ─────────────────────────────────────────────────"))
   (require 'bb-org-capture))
 
 
