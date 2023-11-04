86ebd61e93853165017918a9fa3085fd6c217cd9
bboal <egomet@bboal.com>
2023-11-04 17:33:23 +0000

    No more matching parenthesis not found when typing solo ")" or another similar symbol

--- init.el
+++ init.el
@@ -53,6 +53,7 @@
 ;;; User preferences
 (setq bidi-inhibit-bpa t
       show-paren-delay 0
+      blink-matching-paren nil
       scroll-conservatively 101
       x-stretch-cursor t
       ring-bell-function 'ignore
